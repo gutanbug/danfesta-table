18098fb56fe8b0570bdba533d890badf8aba1b4a
ChaHyeonMin <gutanbug53@gmail.com>
2024-04-29 11:24:50 +0900

    Initial Commit for SpringBoot

---
+++ src/main/java/com/dku/council/danfestatable/DanfestaTableApplication.java
@@ -0,0 +1,13 @@
+package com.dku.council.danfestatable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanfestaTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanfestaTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dku/council/danfestatable/DanfestaTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dku.council.danfestatable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanfestaTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
