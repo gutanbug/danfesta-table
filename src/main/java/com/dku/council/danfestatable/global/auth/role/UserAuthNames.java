7e2f19ff4549c9f5e4323e6f9c3f3493865fb414
ChaHyeonMin <gutanbug53@gmail.com>
2024-04-29 12:29:51 +0900

    feat : UserRole에 사용될 Auth 이름 및 UserAuth 어노테이션 추가

---
+++ src/main/java/com/dku/council/danfestatable/global/auth/role/UserAuthNames.java
@@ -0,0 +1,12 @@
+package com.dku.council.danfestatable.global.auth.role;
+
+public class UserAuthNames {
+
+    public static final String ROLE_USER = "ROLE_USER";
+
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    public static String combine(String... names) {
+        return String.join(",", names);
+    }
+}
