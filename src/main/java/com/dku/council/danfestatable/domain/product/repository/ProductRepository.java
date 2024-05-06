fc5b07cc36b2317d94309911a9e15fdbd5ff25ff
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-06 18:39:39 +0900

    feat : 상품 주문 관리를 위한 ProductOrdersRepository 추가 및 상품 레포지토리 추가

--- src/main/java/com/dku/council/danfestatable/domain/product/repository/ProductRepository.java
+++ src/main/java/com/dku/council/danfestatable/domain/product/repository/ProductRepository.java
@@ -2,6 +2,13 @@ package com.dku.council.danfestatable.domain.product.repository;
 
 import com.dku.council.danfestatable.domain.product.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p from Product p where p.name = :name")
+    Optional<Product> findByName(@Param("name") String name);
 }
