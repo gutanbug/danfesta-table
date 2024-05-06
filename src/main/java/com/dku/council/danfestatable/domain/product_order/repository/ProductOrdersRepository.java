d28e93e3b4374e3a729caa0432ddba85f7329461
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-06 21:56:15 +0900

    feat : orderId로 ProductOrders 찾는 JPQL 레포지토리에 추가

--- src/main/java/com/dku/council/danfestatable/domain/product_order/repository/ProductOrdersRepository.java
+++ src/main/java/com/dku/council/danfestatable/domain/product_order/repository/ProductOrdersRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductOrdersRepository extends JpaRepository<ProductOrders, Long> {
-    @Query("select po from ProductOrders po join po.orders o join o.matchingTable t where t.id = :id")
+    @Query("select po from ProductOrders po join po.orders o join o.matchingTable t where t.id = :id " +
+            "and o.orderStatus != 'CANCELED'")
     List<ProductOrders> findAllByTableId(@Param("id") Long id);
+
+    @Query("select po from ProductOrders po join fetch po.orders o where o.id = :orderId")
+    Optional<ProductOrders> findByOrderId(@Param("orderId") Long orderId);
 }
