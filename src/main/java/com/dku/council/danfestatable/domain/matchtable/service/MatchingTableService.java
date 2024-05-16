6ab69dbc82b4fcdda77476b25907675a085c6517
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-16 14:56:57 +0900

    feat : 매칭 테이블 참가 서비스 로직 추가

--- src/main/java/com/dku/council/danfestatable/domain/matchtable/service/MatchingTableService.java
+++ src/main/java/com/dku/council/danfestatable/domain/matchtable/service/MatchingTableService.java
@@ -5,6 +5,7 @@ import com.dku.council.danfestatable.domain.matchtable.model.dto.response.Respon
 import com.dku.council.danfestatable.domain.matchtable.model.dto.response.SummarizedTableDto;
 import com.dku.council.danfestatable.domain.matchtable.model.entity.MatchingTable;
 import com.dku.council.danfestatable.domain.matchtable.repository.MatchingTableRepository;
+import com.dku.council.danfestatable.domain.user.exception.UserNotFoundException;
 import com.dku.council.danfestatable.domain.user.model.entity.User;
 import com.dku.council.danfestatable.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +34,24 @@ public class MatchingTableService {
         MatchingTable table = repository.findByUserId(userId).orElseThrow(MatchingTableNotFoundException::new);
         List<User> users = userRepository.findAllUsersByTableId(table.getId());
         return new ResponseMyTableDto(table, users.stream().map(User::getName).collect(Collectors.toList()));
+    }
+
+    @Transactional
+    public void joinTable(Long userId, int number) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        if (repository.findByTableNumber(number).isEmpty()) {
+            MatchingTable table = MatchingTable.builder()
+                    .tableNumber(number)
+                    .build();
+            repository.save(table);
+            table.addUser(user);
+            user.setMatchingTable(table);
+        } else {
+            MatchingTable table = repository.findByTableNumber(number).orElseThrow(MatchingTableNotFoundException::new);
+            table.addUser(user);
+            user.setMatchingTable(table);
+        }
     }
 
     // TODO : 하트 결제 로직
