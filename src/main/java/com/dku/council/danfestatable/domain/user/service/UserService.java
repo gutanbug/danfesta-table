8862c25f376038f9376349c0eb14f7910bbe9d69
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-17 17:11:40 +0900

    feat : 직원 호출 서비스 로직 추가

--- src/main/java/com/dku/council/danfestatable/domain/user/service/UserService.java
+++ src/main/java/com/dku/council/danfestatable/domain/user/service/UserService.java
@@ -1,8 +1,12 @@
 package com.dku.council.danfestatable.domain.user.service;
 
+import com.dku.council.danfestatable.domain.matchtable.exception.MatchingTableNotFoundException;
+import com.dku.council.danfestatable.domain.matchtable.model.entity.MatchingTable;
+import com.dku.council.danfestatable.domain.matchtable.repository.MatchingTableRepository;
 import com.dku.council.danfestatable.domain.user.exception.AlreadyUserExistException;
 import com.dku.council.danfestatable.domain.user.exception.UserNotFoundException;
 import com.dku.council.danfestatable.domain.user.model.Enrolled;
+import com.dku.council.danfestatable.domain.user.model.dto.request.RequestCallStaffDto;
 import com.dku.council.danfestatable.domain.user.model.dto.request.RequestLoginDto;
 import com.dku.council.danfestatable.domain.user.model.dto.request.RequestSignupDto;
 import com.dku.council.danfestatable.domain.user.model.dto.response.ResponseLoginDto;
@@ -10,22 +14,32 @@ import com.dku.council.danfestatable.domain.user.model.entity.User;
 import com.dku.council.danfestatable.domain.user.repository.UserRepository;
 import com.dku.council.danfestatable.global.auth.jwt.AuthenticationToken;
 import com.dku.council.danfestatable.global.auth.jwt.JwtProvider;
+import com.dku.council.danfestatable.infra.nhn.sms.service.MMSService;
+import com.dku.council.danfestatable.infra.nhn.sms.service.SMSService;
 import com.dku.council.danfestatable.infra.nhn.sms.service.SMSVerificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
 
+    private static final String MMSTitle = "[직원호출]";
+
     private final UserRepository userRepository;
+    private final MatchingTableRepository tableRepository;
     private final PasswordEncoder passwordEncoder;
     private final SMSVerificationService smsVerificationService;
+    private final MMSService smsService;
     private final JwtProvider jwtProvider;
+    private final MessageSource messageSource;
 
     @Transactional
     public void signUp(RequestSignupDto dto) {
@@ -70,5 +84,13 @@ public class UserService {
 
         AuthenticationToken tokenResponse = jwtProvider.issue(user);
         return new ResponseLoginDto(tokenResponse);
+    }
+
+    public void callStaff(Long userId, RequestCallStaffDto dto) {
+        MatchingTable table = tableRepository.findByUserId(userId).orElseThrow(MatchingTableNotFoundException::new);
+        User adminUser = userRepository.findAll().stream()
+                .filter(u -> u.getUserRole().isAdmin()).findFirst().orElseThrow(UserNotFoundException::new);
+        smsService.sendMMS(MMSTitle, adminUser.getPhone(), messageSource.getMessage("mms.call-staff",
+                new Object[]{table.getTableNumber(), dto.getMessage()}, LocaleContextHolder.getLocale()));
     }
 }
