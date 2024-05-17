718a68d1cc7b69e5369dc43e0704abb1520063d4
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-17 16:18:15 +0900

    feat : Redis에서 키 값으로 쓸 RedisKeys 추가

--- src/main/java/com/dku/council/danfestatable/global/config/redis/RedisKeys.java
+++ src/main/java/com/dku/council/danfestatable/global/config/redis/RedisKeys.java
@@ -1,4 +1,7 @@
 package com.dku.council.danfestatable.global.config.redis;
 
 public class RedisKeys {
+    public static final String KEY_DELIMITER = ":";
+
+    public static final String SIGNUP_AUTH_KEY = "signup-auth";
 }
