5acad6c08b4040f2e094bbe54c1e277704899061
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-21 02:53:44 +0900

    fix : 강제 회원가입 시 유저 / 관리자 나눠서 생성할 수 있도록 수정

--- src/main/java/com/dku/council/danfestatable/domain/admin/controller/TestController.java
+++ src/main/java/com/dku/council/danfestatable/domain/admin/controller/TestController.java
@@ -12,6 +12,7 @@ import com.dku.council.danfestatable.global.auth.jwt.AuthenticationToken;
 import com.dku.council.danfestatable.global.auth.jwt.JwtProvider;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,7 @@ public class TestController {
     private final UserRepository userRepository;
     private final MatchingTableRepository matchingTableRepo;
     private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 임시 로그인
@@ -46,15 +48,20 @@ public class TestController {
                        @RequestParam String gender,
                        @RequestParam String loginId,
                        @RequestParam String password,
-                       @RequestParam Enrolled enrolled) {
+                       @RequestParam Enrolled enrolled,
+                       @RequestParam (defaultValue = "false") boolean setAdmin) {
         User user = User.builder()
                 .name(name)
                 .phone(phone)
                 .gender(gender)
                 .loginId(loginId)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .enrolled(enrolled)
                 .build();
+
+        if (setAdmin) {
+            user.changeToAdmin();
+        }
         userRepository.save(user);
     }
 }
