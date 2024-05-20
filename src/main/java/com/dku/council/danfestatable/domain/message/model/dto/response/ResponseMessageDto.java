2c12778c010bd93eb34a95ae1cf1a1c348adc684
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-21 02:55:15 +0900

    fix : 메시지 ResponseDto에 createdAt 필드 추가

--- src/main/java/com/dku/council/danfestatable/domain/message/model/dto/response/ResponseMessageDto.java
+++ src/main/java/com/dku/council/danfestatable/domain/message/model/dto/response/ResponseMessageDto.java
@@ -3,6 +3,9 @@ package com.dku.council.danfestatable.domain.message.model.dto.response;
 import com.dku.council.danfestatable.domain.message.model.entity.Message;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Getter
 public class ResponseMessageDto {
 
@@ -12,9 +15,17 @@ public class ResponseMessageDto {
 
     private final String message;
 
+    private final String createdAt;
+
     public ResponseMessageDto(Message message) {
         this.senderTableNumber = message.getSenderTable().getTableNumber();
         this.heartCount = message.getSendHeartCount();
         this.message = message.getMessage();
+        this.createdAt = cleanTime(message.getCreatedAt());
+    }
+
+    private String cleanTime(LocalDateTime time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+        return time.format(formatter);
     }
 }
